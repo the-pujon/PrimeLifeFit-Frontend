035b74583dbd47818662c74e826e0bc0bdd71f5b
Pujon Das Auvi <auvipujondas@gmail.com>
2024-09-26 13:59:05 +0600

    sign up added

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -11,7 +11,7 @@ import { toast } from "sonner";
 import { RootState } from "../store";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:5000/api",
+  baseUrl: "http://localhost:50000/api",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.token;
