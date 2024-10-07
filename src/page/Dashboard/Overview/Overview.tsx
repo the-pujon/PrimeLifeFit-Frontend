c55ed135f259c6341af83f9033fe60f82011e2f2
Pujon Das Auvi <auvipujondas@gmail.com>
2024-10-07 06:45:26 +0600

    overview api implemented

--- src/page/Dashboard/Overview/Overview.tsx
+++ src/page/Dashboard/Overview/Overview.tsx
@@ -1,20 +1,66 @@
 import React from 'react'
 import { Card,CardContent,CardHeader,CardTitle } from "@/components/ui/card"
-import { Bar,BarChart,ResponsiveContainer,XAxis,YAxis } from "recharts"
+import { Bar,BarChart,ResponsiveContainer,XAxis,YAxis,Tooltip,TooltipProps } from "recharts"
 import { motion } from 'framer-motion'
 import { ShoppingBag,Users,Package,DollarSign } from 'lucide-react'
+import { useGetAllOrdersQuery } from '@/redux/features/order/orderApi'
+import { useGetAllProductsQuery } from '@/redux/features/product/productApi'
+import { useGetAllUsersQuery } from '@/redux/features/auth/authApi'
+import Loading from '@/components/ui/Loading'
+import { Order } from '@/types/Orders'
 
-// Mock data for charts
-const monthlySales = [
-    { name: "Jan",total: 1200 },
-    { name: "Feb",total: 2100 },
-    { name: "Mar",total: 1800 },
-    { name: "Apr",total: 2400 },
-    { name: "May",total: 2800 },
-    { name: "Jun",total: 3200 },
-]
+interface ChartData {
+    name: string;
+    total: number;
+    orders: number;
+}
 
 const Overview: React.FC = () => {
+    const { data: orders,isLoading: ordersLoading } = useGetAllOrdersQuery(undefined)
+    const { data: products,isLoading: productsLoading } = useGetAllProductsQuery('')
+    const { data: users,isLoading: usersLoading } = useGetAllUsersQuery(undefined)
+
+    const isLoading = ordersLoading || productsLoading || usersLoading
+
+    if (isLoading) {
+        return <Loading />
+    }
+
+    const totalRevenue = orders?.data.reduce((sum: number,order: Order) => sum + order.totalAmount,0) || 0
+    const totalOrders = orders?.data.length || 0
+    const totalCustomers = users?.data.length || 0
+    const totalProducts = products?.data.length || 0
+
+    const monthlySales = orders?.data.reduce((acc: Record<string,number>,order: Order) => {
+        const date = new Date(order.createdAt)
+        const month = date.toLocaleString('default',{ month: 'short' })
+        acc[month] = (acc[month] || 0) + order.totalAmount
+        return acc
+    },{})
+
+    const chartData: ChartData[] = Object.entries(monthlySales || {}).map(([name,total]) => ({
+        name,
+        total: total as number,
+        orders: orders?.data.filter((order: Order) => {
+            const orderMonth = new Date(order.createdAt).toLocaleString('default',{ month: 'short' })
+            return orderMonth === name
+        }).length || 0
+    }))
+
+    const CustomTooltip: React.FC<TooltipProps<number,string>> = ({ active,payload,label }: TooltipProps<number,string>) => {
+        if (active && payload && payload.length) {
+            const data = payload[0].payload as ChartData;
+            return (
+                <div className="custom-tooltip bg-white p-4 border border-gray-200 rounded shadow">
+                    <p className="label font-bold">{`${label}`}</p>
+                    <p className="total text-blue-600">{`Total: $${data.total.toFixed(2)}`}</p>
+                    <p className="orders text-green-600">{`Orders: ${data.orders}`}</p>
+                </div>
+            );
+        }
+        return null;
+    };
+
     return (
         <motion.div
             initial={{ opacity: 0 }}
@@ -39,7 +85,7 @@ const Overview: React.FC = () => {
                         <DollarSign className="h-4 w-4 text-muted-foreground" />
                     </CardHeader>
                     <CardContent>
-                        <div className="text-2xl font-bold">$45,231.89</div>
+                        <div className="text-2xl font-bold">${totalRevenue.toFixed(2)}</div>
                         <p className="text-xs text-muted-foreground">
                             +20.1% from last month
                         </p>
@@ -48,26 +94,26 @@ const Overview: React.FC = () => {
                 <Card>
                     <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                         <CardTitle className="text-sm font-medium">
-                            New Orders
+                            Total Orders
                         </CardTitle>
                         <ShoppingBag className="h-4 w-4 text-muted-foreground" />
                     </CardHeader>
                     <CardContent>
-                        <div className="text-2xl font-bold">+573</div>
+                        <div className="text-2xl font-bold">{totalOrders}</div>
                         <p className="text-xs text-muted-foreground">
-                            +201 since last hour
+                            +180 since last hour
                         </p>
                     </CardContent>
                 </Card>
                 <Card>
                     <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                         <CardTitle className="text-sm font-medium">
-                            New Customers
+                            Total Customers
                         </CardTitle>
                         <Users className="h-4 w-4 text-muted-foreground" />
                     </CardHeader>
                     <CardContent>
-                        <div className="text-2xl font-bold">+2350</div>
+                        <div className="text-2xl font-bold">{totalCustomers}</div>
                         <p className="text-xs text-muted-foreground">
                             +180.1% from last month
                         </p>
@@ -76,12 +122,12 @@ const Overview: React.FC = () => {
                 <Card>
                     <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                         <CardTitle className="text-sm font-medium">
-                            Inventory Stock
+                            Total Products
                         </CardTitle>
                         <Package className="h-4 w-4 text-muted-foreground" />
                     </CardHeader>
                     <CardContent>
-                        <div className="text-2xl font-bold">12,234</div>
+                        <div className="text-2xl font-bold">{totalProducts}</div>
                         <p className="text-xs text-muted-foreground">
                             +7% from last month
                         </p>
@@ -96,7 +142,7 @@ const Overview: React.FC = () => {
                     </CardHeader>
                     <CardContent className="pl-2">
                         <ResponsiveContainer width="100%" height={350}>
-                            <BarChart data={monthlySales}>
+                            <BarChart data={chartData}>
                                 <XAxis
                                     dataKey="name"
                                     stroke="#888888"
@@ -111,6 +157,7 @@ const Overview: React.FC = () => {
                                     axisLine={false}
                                     tickFormatter={(value) => `$${value}`}
                                 />
+                                <Tooltip content={<CustomTooltip />} />
                                 <Bar
                                     dataKey="total"
                                     fill="#111827"
@@ -126,39 +173,19 @@ const Overview: React.FC = () => {
                     </CardHeader>
                     <CardContent>
                         <div className="space-y-8">
-                            <div className="flex items-center">
-                                <ShoppingBag className="mr-2 h-4 w-4 text-muted-foreground" />
-                                <div className="ml-4 space-y-1">
-                                    <p className="text-sm font-medium leading-none">
-                                        New order #1234
-                                    </p>
-                                    <p className="text-sm text-muted-foreground">
-                                        5 minutes ago
-                                    </p>
+                            {orders?.data.slice(0,3).map((order: Order) => (
+                                <div key={order._id} className="flex items-center">
+                                    <ShoppingBag className="mr-2 h-4 w-4 text-muted-foreground" />
+                                    <div className="ml-4 space-y-1">
+                                        <p className="text-sm font-medium leading-none">
+                                            New order #{order._id}
+                                        </p>
+                                        <p className="text-sm text-muted-foreground">
+                                            {new Date(order.createdAt).toLocaleString()}
+                                        </p>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="flex items-center">
-                                <Users className="mr-2 h-4 w-4 text-muted-foreground" />
-                                <div className="ml-4 space-y-1">
-                                    <p className="text-sm font-medium leading-none">
-                                        New customer registered
-                                    </p>
-                                    <p className="text-sm text-muted-foreground">
-                                        15 minutes ago
-                                    </p>
-                                </div>
-                            </div>
-                            <div className="flex items-center">
-                                <Package className="mr-2 h-4 w-4 text-muted-foreground" />
-                                <div className="ml-4 space-y-1">
-                                    <p className="text-sm font-medium leading-none">
-                                        Product "XYZ" is low in stock
-                                    </p>
-                                    <p className="text-sm text-muted-foreground">
-                                        1 hour ago
-                                    </p>
-                                </div>
-                            </div>
+                            ))}
                         </div>
                     </CardContent>
                 </Card>
